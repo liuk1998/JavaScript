463bae97de38cb7041bda9e2a214219011b793eb
liuk <kekeliu_job@163.com>
2022-03-09 10:24:46 +0800

    更新

--- js/function.js
+++ js/function.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-03-01 15:46:01
- * @LastEditTime: 2022-03-01 16:32:46
+ * @LastEditTime: 2022-03-09 10:23:32
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /JavaScript/js/function.js
@@ -27,3 +27,17 @@ Try();
 (() => {
   console.log('立即执行函数');
 })();
+
+
+// ! 递归 : 要给出口。
+function Iterator(num) {
+  num++;
+  console.log(num);
+  if(num < 10) {
+    Iterator(num);
+  };
+};
+Iterator(1);
+
+
+// ! 惰性函数表示函数执行的分支只会在函数第一次调用的时候执行。后续我们所使用的就是这个函数执行的结果。
